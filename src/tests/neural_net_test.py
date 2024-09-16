6ecfbeab5cf0a0894d5d768b12c253744b1336de
pakkanep <patrikpakkanen9@gmail.com>
2024-09-16 19:14:09 +0300

    Initialized Poetry and added test stuff

--- src/tests/neural_net_test.py
+++ src/tests/neural_net_test.py
@@ -5,9 +5,21 @@ class TestNeuralNet(unittest.TestCase):
     def setUp(self) -> None:
         return super().setUp()
     
-    def test_backpropagation():
+    def test_backpropagation(self):
         pass
 
-    def test_mini_batch():
+    def test_mini_batch(self):
         pass
         
+    def test_gradients_are_non_zero_and_loss_decreases(self):
+        pass
+
+    def test_network_overfits_to_a_small_dataset(self):
+        pass
+    
+
+    def test_hello_world(self):
+        self.assertEqual("Hello world", "Hello world")
+
+    #def test_order_of_samples_in_batch_doesnt_affect_output
+    #def test_all_network_layers_change_after_each_optimizer_step
