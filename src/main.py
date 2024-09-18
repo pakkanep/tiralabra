Found anf fixed the bug in backprop algo.

src/main.py
@@ -20,7 +20,7 @@ def load_test_data():
 
 
 if __name__ == "__main__":
-    sizes = [784, 40, 10]
+    sizes = [784, 50, 10]
     neuroverkko = NeuralNet(sizes)
     training, validation, testing = ld.load_data()
     test_data = load_test_data()
@@ -28,13 +28,15 @@ if __name__ == "__main__":
     batch_size = 10
     n = len(training)
 
-
+    rundit =0
     for round in range(10):
         random.shuffle(training)
         for idx in range(0, n, batch_size):
+            rundit+=1
             mini_batch = training[idx:idx+batch_size]
             neuroverkko.mini_batch(mini_batch, learning_rate)
         
+        print(rundit)
         summa = 0
         for testi in testing:
             if np.argmax(neuroverkko.neuralnet_output(testi[0])) == testi[1]:

src/neural_net.py
@@ -26,95 +26,45 @@ class NeuralNet():
 
 
     def mini_batch(self, mini_batch, learning_rate):
-        """
-            Compute the gradient for many training examples
-            
-            For each layer update the WEIGHTS with formula: w^l - n/m * sum(outputerror^x,l * (a^x,(l-1)^T)
-            where:
-                w^l = weights of the layer
-                n = learning rate
-                m = size of minibatch
-                outputerror^x,l = output error of layer l in vector form
-                (a^x,(l-1)^T = Transpose of activations of neurons of training example x on layer l-1 in matrix form
 
-            For each layer update the biases with formula: b^l - n/m * sum(δ^l)
-            where:
-                δ^l = error vectors
-
-        """
-        grad_biases = [np.zeros(x.shape) for x in self.biases] # sum of the cost func
-        grad_weights = [np.zeros(x.shape) for x in self.weights] # same goes for weights
+        grad_biases = [np.zeros(b.shape) for b in self.biases] # sum of the cost func
+        grad_weights = [np.zeros(w.shape) for w in self.weights] # same goes for weights
         
 
         for X, Y in mini_batch:
             nudges_b, nudges_w = self.backpropagation(X, Y)
             # call back propagation that calculates and returns the gradients
             # increase or decrease the weights and biases to minimize the cost func 
-            grad_biases = [b + nudge_b for b, nudge_b in zip(grad_biases, nudges_b)]
-            grad_weights = [w + nudge_w for w, nudge_w in zip(grad_biases, nudges_w)]
+            grad_biases = [gb + nudge_b for gb, nudge_b in zip(grad_biases, nudges_b)]
+            grad_weights = [gw + nudge_w for gw, nudge_w in zip(grad_biases, nudges_w)]
 
         
         m = len(mini_batch)
         
-        #update the weights and biases
         self.weights = [w - (learning_rate / m) * grad_w for w, grad_w in zip(self.weights, grad_weights)]
-
         self.biases = [b - (learning_rate / m) * grad_b for b, grad_b in zip(self.biases, grad_biases)]
 
 
 
     def backpropagation(self, x, y):
-        """
-            Computes the gradient of the cost function for a single training example
-            
-            returns: gradients
 
-            1. Input:
-                x = 28x28 pixel image changed to a vector of shape (784, 1).
-                the vector items are the first layers(input layers) neurons activation values between 0.0 - 1.0
-                y = desired output which we then use to calculate the output error
+        activations = [x] 
+        z_vectors = []
+        activation = x 
 
-            2. Feed forward:
-                For each l = 2, 3,..., L compute
-                z^l = w^l * a^l-1 + b^l 
-                a^l = sigmoid(z^l) 
-
-            Compute the error vectors δ^l backward, starting from the final layer.    
-            
-            3. Output error:
-                compute the vector delta (δ^L) = prime_sigmoid(z^L)
-
-            4. Backpropagate the error:
-                For each  l=L-1,L-2,...., 2
-
-            5. Output:
-                nabla_biases: layer by layer list where each array represents the gradient of the cost function with respect to the biases for a layer.
-                nabla_weights = layer by layer list where each array represents the gradient of the cost function with respect to the weights for a layer
-
-        
-        """
-        # feed forward loop
-        activations = [x] # x contains the activations from the input layer as vector shape of 784,
-        weighted_inputs = [] # save weighted inputs z^l of all layers
-        activation = activations[0] # sets the input layer activations to start counting the activations
-
-        for w, b in zip(self.weights, self.biases): # not including the input layer
+        for b, w in zip(self.biases, self.weights): # not including the input layer
             weighted_input = np.dot(w, activation) + b # z^l = w^l * a^l-1 + b^l 
-            weighted_inputs.append(weighted_input)
+            z_vectors.append(weighted_input)
+
             activation = sigmoid(weighted_input) # a^l = sigmoid(z^l)
             activations.append(activation) 
             
         
-        # calculate the error of last layer to be able to calc the error of previous layers
-        grad_biases = [np.zeros(x.shape) for x in self.biases]
-        grad_weights = [np.zeros(x.shape) for x in self.weights]
+        grad_biases = [np.zeros(b.shape) for b in self.biases]
+        grad_weights = [np.zeros(w.shape) for w in self.weights]
 
-        delta = (activations[-1] - y) * sigmoid_prime(weighted_input[-1]) # (δ^L)
-        
-        
-        #REMEMBER TO DELETE PRINT
-        #print("prints from backprop algo delta\n", delta) 
-        
+        C_x = activations[-1] - y
+        delta = C_x * sigmoid_prime(z_vectors[-1]) # (δ^L)
         
         
         grad_biases[-1] = delta
@@ -124,18 +74,19 @@ class NeuralNet():
         # backpropagation loop
         
         for layer in range(2, self.layers): # from final layer to first layer but not input layer
-            delta = np.dot(self.weights[-layer+1].transpose(), delta) * sigmoid_prime(weighted_inputs[-layer]) 
+            zl = z_vectors[-layer]
+            derivative = sigmoid_prime(zl)
+            delta = np.dot(self.weights[-layer+1].transpose(), delta) * derivative
+            
             grad_biases[-layer] = delta
             grad_weights[-layer] = np.dot(delta, activations[-layer-1].transpose())
 
         return (grad_biases, grad_weights)
     
 
-
-
 def sigmoid(z):
     return 1.0 / (1.0 + np.exp(-z))
 
 def sigmoid_prime(z):
-    return sigmoid(z)*(1-sigmoid(z))
+    return sigmoid(z) * (1 - sigmoid(z))
 