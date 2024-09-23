Added gradient checking that tests backpropagation algorithm. Does not currenlty pass because of a bug in calculating gradient for biases in either backprop algo or the gradient checkin itself

src/neural_net.py
@@ -5,7 +5,7 @@ import numpy as np
 
 class NeuralNet():
     def __init__(self, layers:list):
-        self.layers = len(layers)
+        self.n_layers = len(layers)
         self.weights = [np.random.randn(y, x) for x, y in zip(layers[:-1], layers[1:])]
         self.biases = [np.random.randn(y, 1) for y in layers[1:]]
 
@@ -44,7 +44,7 @@ class NeuralNet():
         Calculates the MSE 
         """
         y_hat = self.neuralnet_output(x)
-        return 0.5 * (np.sum(y_hat-y)**2)
+        return 0.5 * np.sum((y_hat-y)**2)
 
 
     def mini_batch(self, mini_batch, learning_rate):
@@ -74,7 +74,7 @@ class NeuralNet():
         Calculates the gradient vectors
         """
 
-        activations = [x] 
+        activations_list = [x] 
         z_vectors = []
         activation = x 
 
@@ -83,29 +83,30 @@ class NeuralNet():
             z_vectors.append(weighted_input)
 
             activation = sigmoid(weighted_input) # a^l = sigmoid(z^l)
-            activations.append(activation) 
+            activations_list.append(activation) 
 
 
         grad_biases = [np.zeros(b.shape) for b in self.biases]
         grad_weights = [np.zeros(w.shape) for w in self.weights]
 
-        C_x = activations[-1] - y
+        C_x = activations_list[-1] - y
         delta = C_x * sigmoid_prime(z_vectors[-1]) # (δ^L)
 
 
         grad_biases[-1] = delta
-        grad_weights[-1] = np.dot(delta, activations[-2].transpose())
+        grad_weights[-1] = np.dot(delta, activations_list[-2].transpose())
 
 
         # backpropagation loop
 
-        for layer in range(2, self.layers): # from final layer to first layer but not input layer
+        for layer in range(2, self.n_layers): # from final layer to first layer but not input layer
             zl = z_vectors[-layer]
             derivative = sigmoid_prime(zl)
+
             delta = np.dot(self.weights[-layer+1].transpose(), delta) * derivative
 
             grad_biases[-layer] = delta
-            grad_weights[-layer] = np.dot(delta, activations[-layer-1].transpose())
+            grad_weights[-layer] = np.dot(delta, activations_list[-layer-1].transpose())
 
         return (grad_biases, grad_weights)
 

src/tests/neural_net_test.py
@@ -25,8 +25,50 @@ class TestNeuralNet(unittest.TestCase):
         Calculate numerical gradient and compare it to the analytical gradient
         that backprop calculates to ensure that it calculates them correctly.
         """
-        pass
+        x, y = self.data[0][0][0], self.data[0][0][1]
+        epsilon = 1e-5
 
+        nabla_b, nabla_w = self.neural_net.backpropagation(x, y)
+        num_nabla_b = [np.zeros(b.shape) for b in self.neural_net.biases]
+        num_nabla_w = [np.zeros(w.shape) for w in self.neural_net.weights]
+
+        
+        for l in range(len(self.neural_net.biases)):
+            for i in range((self.neural_net.biases[l].shape[0])):
+                org_b = self.neural_net.biases[l][i]
+
+                self.neural_net.biases[l][i] = org_b + epsilon
+                cost_plus = self.neural_net.cost_function(x, y)
+                self.neural_net.biases[l][i] = org_b - epsilon
+                cost_minus = self.neural_net.cost_function(x, y)
+                self.neural_net.biases[l][i] = org_b
+                num_nabla_b[l][i] = (cost_plus - cost_minus) / (2 * epsilon)
+
+        for l in range(len(self.neural_net.weights)):
+            for i in range(self.neural_net.weights[l].shape[0]):
+                for j in range(self.neural_net.weights[l].shape[1]):
+                    org_w = self.neural_net.weights[l][i, j]
+
+                    self.neural_net.weights[l][i, j] = org_w + epsilon
+                    cost_plus = self.neural_net.cost_function(x, y)
+
+                    self.neural_net.weights[l][i, j] = org_w - epsilon
+                    cost_minus = self.neural_net.cost_function(x, y)
+
+                    self.neural_net.weights[l][i, j] = org_w
+                    num_nabla_w[l][i, j] = (cost_plus - cost_minus) / (2 * epsilon)
+        
+        rel_diffs = []
+
+        for l in range(len(nabla_b)):
+            difference = (np.linalg.norm(num_nabla_b[l] - nabla_b[l])) / (np.linalg.norm(num_nabla_b[l]) + np.linalg.norm(nabla_b[l]))
+            rel_diffs.append(difference)
+
+        for l in range(len(nabla_w)):
+            difference = np.linalg.norm(num_nabla_w[l] - nabla_w[l]) / (np.linalg.norm(num_nabla_w[l]) + np.linalg.norm(nabla_w[l]))
+            rel_diffs.append(difference)
+
+        return self.assertGreater(10**-7, max(rel_diffs))
 
 
     def test_mini_batch(self):